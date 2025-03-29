1210ab3b715b65969b8f0d1f767a5180ba46e0f7
davidchkh5 <daviti.chkhaidze5@gmail.com>
2025-03-29 12:55:42 +0400

    qwe

--- newWebProject/Functions.py
+++ newWebProject/Functions.py
@@ -1,7 +1,15 @@
+import os
+
 def get_todos(filepath ="todos.txt"):
     """ Read a text file and return the list of
     to-do items
     """ #when we type help(get_todos) the document string that is top of the function will appear its used to show what current function do
+
+    if not os.path.exists(filepath):
+        with open(filepath, 'w') as file:
+            pass
+
+
     with open(filepath,'r') as file:
         todos_local = file.readlines()
     return todos_local
